b66d26df40f003d1ba72994c0370057954f46ed4
sohilakhalil <sohilakhalil283@gmail.com>
2025-09-22 01:09:28 +0300

    fix: correct Home import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Report from "./pages/Report";
 import TrackReportPage from "./pages/Track";
 
